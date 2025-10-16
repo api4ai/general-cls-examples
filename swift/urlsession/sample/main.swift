9a1d0dc32beea098ec1ab6fa9ee276ef09f5f305
Vladislav Gubarev <gubarev@api4.ai>
2025-10-16 17:18:21 +0300

    Use shorted and faster link to sample images

--- swift/urlsession/sample/main.swift
+++ swift/urlsession/sample/main.swift
@@ -47,7 +47,7 @@ let OPTIONS = [
 
 
 // Prepare http body with image or url.
-let image = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : "https://storage.googleapis.com/api4ai-static/samples/general-cls-2.jpg"
+let image = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : "https://static.api4.ai/samples/general-cls-2.jpg"
 var httpBody: Data;
 if (image.contains("://")) {
     // POST image via URL.
